648c18e48d29546a71acf7af7f40395db1eb4d47
fishdown <119657362+fishdown@users.noreply.github.com>
2026-01-01 15:18:05 +0800

    Update 18-m3u8.js

--- Js/18-m3u8.js
+++ Js/18-m3u8.js
@@ -19,6 +19,7 @@ mkvpipurl://
 
 // 当前请求 URL
 const reqUrl = $request.url;
+console.log("请求Url：");
 console.log(reqUrl);
 // 仅处理 m3u8
 if (!reqUrl || !/\.m3u8(\?|$)/i.test(reqUrl)) {
@@ -28,10 +29,8 @@ if (!reqUrl || !/\.m3u8(\?|$)/i.test(reqUrl)) {
 
 // 从 argument 获取 scheme
 const scheme = ($argument.sch || "").trim();
-if (!scheme) {
-  $done({});
-  return;
-}
+console.log("获取到的scheme：");
+console.log(scheme);
 
 // 读取上一次 m3u8
 const cacheKey = "LAST_M3U8_URL";
@@ -48,6 +47,7 @@ $persistentStore.write(reqUrl, cacheKey);
 
 // 拼接跳转链接
 const jumpUrl = scheme + reqUrl;
+console.log("跳转Url：");
 console.log(jumpUrl);
 // 通知附件（点击跳转 + 复制）
 const attach = {
