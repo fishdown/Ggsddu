661b02c5dbd2c662c2a7033c98cf692901d6a66c
fishdown <119657362+fishdown@users.noreply.github.com>
2026-08-05 17:27:02 +0800

    Update 18-m3u8.js

--- Js/18-m3u8.js
+++ Js/18-m3u8.js
@@ -4,34 +4,6 @@
 #!author = fishdown[https://github.com/fishdown/Ggsddu]
 #!icon = https://raw.githubusercontent.com/fishdown/Icon/refs/heads/master/app/porn.png
 
-[Rule]
-#-----------------------------------------------------------------m3u8地址
-# 萝莉岛 妻友社区
-DOMAIN-SUFFIX,cloudfront.net,PROXY
-# 50度灰，海角社区，91porn短视频
-# long.*.cn 走直连
-# MissAV
-DOMAIN-SUFFIX,cdn2020.com,PROXY
-DOMAIN,surrit.com,PROXY
-# 糖心
-DOMAIN,tth.txh069.com,PROXY
-# xx9
-DOMAIN,babe.babeshop.xyz,PROXY
-#ikm26 
-DOMAIN,m2.kdamao.com,PROXY
-# 麻豆社
-DOMAIN,dash.madou.club,PROXY
-# pear
-DOMAIN-SUFFIX,pear2.cc,PROXY
-# mmav
-DOMAIN,kwmdmmsp.hongtaitanghua.com,PROXY
-# 红杏视频
-DOMAIN,bf4.qrtuv.com,PROXY
-# 91工厂
-DOMAIN-SUFFIX,kyxcom.com,PROXY
-# 好色tv
-DOMAIN-SUFFIX,hdcdn.online,PROXY
-
 [MitM]
 hostname = *.cloudfront.net,long.*.cn,*.cdn2020.com,surrit.com,babe.babeshop.xyz,m2.kdamao.com,dash.madou.club,*.pear2.cc,kwmdmmsp.hongtaitanghua.com,bf4.qrtuv.com,*.kyxcom.com,*.hdcdn.online
 
@@ -40,8 +12,8 @@ http-request \.m3u8 script-path=https://raw.githubusercontent.com/fishdown/Ggsdd
 
 [Argument]
 vid = select,"MKVPiP","lenna", "SenPlayer", "SenPlayer-dl", "Infuse", "Fileball", "VidHub", "Alook", "VLC", "KMPlayer", "IINA", "NPlayer", "Safari",tag=选取播放器,desc=
-userscheme = input, "", tag=自定义Scheme, desc=
-ucode  = select,"yes","no",tag=URL编码,desc=
+userscheme = input, "", tag=自定义Scheme/优先级最高, desc=
+ucode  = select,"yes","no",tag=URL编码/不智能自行判断,desc=
 
 */
 
